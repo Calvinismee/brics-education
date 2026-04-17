d87174b3a9b2bea778bf15f9d132c270b2d02adb
Calvinismee <juliuscalvin_kurniadi@apps.ipb.ac.id>
2026-04-17 12:42:30 +0700

    Update readme

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,14 @@
 <?php
 
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    DB::select('SELECT 1');
+})->daily();
